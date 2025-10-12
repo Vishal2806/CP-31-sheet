4ea32d980450236b7b00fd82bd957ec5ac8e811a
vishal vishwakarma <vishalv2806@gmail.com>
2025-10-12 06:03:46 +0530

    updated

--- 800 rating/converinwater.cpp
+++ 800 rating/converinwater.cpp
@@ -1,8 +1,36 @@
-//Hello i am Vishal Vishwakarma
-#include<bits/stdc++.h>
+// Hello i am Vishal Vishwakarma
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
- int t;
- cin>>t;
- while(t--)
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string arr;
+        cin >> arr;
+
+        int cntOfEmptyCells = 0;
+        bool isTrue = false;
+
+        for (int i = 1; i < n - 1; i++)
+        {
+
+            if (arr[i - 1] == '.' && arr[i] == '.' && arr[i + 1] == '.')
+            {
+                isTrue = true;
+            }
+        }
+        for(int i =0;i<n;i++){
+             if (arr[i] == '.')
+                cntOfEmptyCells++;
+        }
+        if (isTrue == true) // ✅ Corrected this line
+            cout  << 2 << endl;
+        else
+            cout << cntOfEmptyCells << endl;
+    }
 }
